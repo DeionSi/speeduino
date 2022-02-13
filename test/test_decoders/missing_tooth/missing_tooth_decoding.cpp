f557cc96652de8d003a56c63abbbc93c3cee28e4
DeionSi <DeionSi@users.noreply.github.com>
2022-02-13 22:04:25 +0100

    All tests as timed tests

--- test/test_decoders/missing_tooth/missing_tooth_decoding.cpp
+++ test/test_decoders/missing_tooth/missing_tooth_decoding.cpp
@@ -8,22 +8,6 @@
 const byte unityMessageLength = 200;
 char unityMessage[unityMessageLength];
 
-void testSync() {
-  TEST_ASSERT_EQUAL(currentDecodingTest->expectedSync, currentStatus.hasSync);
-}
-void testHalfSync() {
-  TEST_ASSERT_EQUAL(currentDecodingTest->expectedHalfSync, BIT_CLEAR(currentStatus.status3, BIT_STATUS3_HALFSYNC));
-}
-void testSyncLossCount() {
-  TEST_ASSERT_EQUAL(currentDecodingTest->expectedSyncLossCount, currentStatus.syncLossCounter);
-}
-void testRPM() {
-  TEST_ASSERT_EQUAL(currentDecodingTest->expectedRPM, getRPM());
-}
-void testRevolutionCounter() {
-  TEST_ASSERT_EQUAL(currentDecodingTest->expectedRevolutionCount, currentStatus.startRevolutions); // How to count?
-}
-
 void testDecoding_reset() {
   //This is from Speeduinos main loop which check for engine stall/turn off and resets a bunch decoder related values
   //TODO: Use a function in speeduino to do this
@@ -35,6 +19,15 @@ void testDecoding_reset() {
   currentStatus.startRevolutions = 0;
   toothSystemCount = 0;
   secondaryToothCount = 0;
+}
+
+void delayUntil(uint32_t time) {
+  if (micros() < time) { // No delay if we have already passed
+    if (time >= 12000) { // Make sure we don't compare against negative time
+      while (micros() < time - 12000) { delay(10); } // Delay in MILLIseconds until we can delay in MICROseconds
+    }
+    delayMicroseconds(time - micros()); // Remaining delay in MICROseconds
+  }
 }
 
 void testMissingToothDecoding_execute() {
@@ -54,92 +47,101 @@ void testMissingToothDecoding_execute() {
 
   // TODO: Everything below is to do:
 
-  uint32_t triggerLog[currentDecodingTest->primaryTriggerPatternExecuteCount];
-  uint32_t nextTrigger = micros();
-  uint16_t timingOffsetFrom0 = nextTrigger;
+  uint32_t triggerLog[currentDecodingTest->primaryTriggerPatternExecuteCount + currentDecodingTest->timedTestsCount];
+  uint32_t nextTrigger = 0;
+  uint32_t timingOffsetFrom0 = micros();
   byte timedTestPos = 0;
+  byte triggerLogPos = 0;
   
   for (int i = 0, patternPosition = currentDecodingTest->primaryTriggerPatternStartPos; i < currentDecodingTest->primaryTriggerPatternExecuteCount;) {
 
-    triggerLog[i] = micros();
+    delayUntil(nextTrigger + timingOffsetFrom0);
+
+    triggerLog[triggerLogPos] = micros();
+    triggerLogPos++;
 
     triggerHandler();
+
+    //UnityPrint("trigger ");
+    //UnityPrintNumberUnsigned(triggerLog[triggerLogPos-1]);
+    //UNITY_PRINT_EOL();
 
     //snprintf(unityMessage, unityMessageLength, "getCrankAngle %d / hasSync %d", getCrankAngle(), currentStatus.hasSync);
     //UNITY_PRINT_EOL();
     
     if (patternPosition >= currentDecodingTest->primaryTriggerPatternCount) { patternPosition = 0; }
+    uint32_t previousTrigger = nextTrigger;
     nextTrigger += currentDecodingTest->primaryTriggerPattern[patternPosition];
 
     i++;
     patternPosition++;
 
     // Do any timed tests before we wait for the next trigger
-    while (timedTestPos < currentDecodingTest->timedTestsCount && currentDecodingTest->timedTests[timedTestPos].time + timingOffsetFrom0 < nextTrigger) {
+    while (timedTestPos < currentDecodingTest->timedTestsCount
+            && currentDecodingTest->timedTests[timedTestPos].time < nextTrigger
+            && currentDecodingTest->timedTests[timedTestPos].time >= previousTrigger
+            ) {
+      
+      // Delay until test target time
+      delayUntil(currentDecodingTest->timedTests[timedTestPos].time + timingOffsetFrom0);
+
+      triggerLog[triggerLogPos] = micros();
+      triggerLogPos++;
+
       currentDecodingTest->timedTests[timedTestPos].prepareResult();
       timedTestPos++;
-    }
 
-    // Delay until next
-    while (micros() < nextTrigger-12000) {
-      delay(10);
+      //snprintf(unityMessage, unityMessageLength, "interval %lu", displayTime);
+      UnityPrint("trigger test");
+      UNITY_PRINT_EOL();
     }
-    delayMicroseconds(nextTrigger - micros());
 
   }
 
   // Run remaining tests that should happen at end
-  while (timedTestPos < currentDecodingTest->timedTestsCount && currentDecodingTest->timedTests[timedTestPos].time + timingOffsetFrom0 < nextTrigger) {
+  while (timedTestPos < currentDecodingTest->timedTestsCount) {
     currentDecodingTest->timedTests[timedTestPos].prepareResult();
     timedTestPos++;
   }
 
-
   // Show a little log of times
   uint32_t lastTriggerTime = 0;
-  for (uint32_t time : triggerLog) {
-    uint32_t displayTime = 0;
-    if (lastTriggerTime > 0) { displayTime = time - lastTriggerTime; }
-    else { displayTime = time; }
-    lastTriggerTime = time;
+  for (int i = 0; i < triggerLogPos; i++) {
+    uint32_t displayTime = triggerLog[i] - lastTriggerTime;
+    lastTriggerTime = triggerLog[i];
 
-    snprintf(unityMessage, unityMessageLength, "interval %lu", displayTime);
+    snprintf(unityMessage, unityMessageLength, "time %lu timefromstart %lu interval %lu", triggerLog[i], triggerLog[i] - timingOffsetFrom0, displayTime);
     UnityPrint(unityMessage);
     UNITY_PRINT_EOL();
   }
 
 }
 
-
-
-void outputTests() {
-
-  //TODO: What are the expected decoder outputs?
-  RUN_TEST(testSync);
-  RUN_TEST(testHalfSync);
-  RUN_TEST(testSyncLossCount);
-  //TODO: move calculation of expected RPM to here
-  RUN_TEST(testRPM);
-  RUN_TEST(testRevolutionCounter);
+static void run_test() {
+  TEST_ASSERT_INT_WITHIN(currentTimedTest->delta, currentTimedTest->expected, currentTimedTest->result);
 }
+
+//TODO: What are the expected decoder outputs?
 
 void testMissingToothDecoding() {
   // Perform the test
   for (auto testData : decodingTests) {
     currentDecodingTest = &testData;
 
-    uint32_t rotationTime = 0;
-    for (int i; i < currentDecodingTest->primaryTriggerPatternCount; i++) { rotationTime += currentDecodingTest->primaryTriggerPattern[i]; }
-    currentDecodingTest->expectedRPM = 60000000L / rotationTime;
+    // Do this to not get poisoned by the previous test name on this INFO
+    Unity.CurrentTestName = NULL;
 
     UnityMessage(currentDecodingTest->name, __LINE__);
     testMissingToothDecoding_execute();
-    outputTests();
 
     for (int i = 0; i < currentDecodingTest->timedTestsCount; i++) {
       currentTimedTest = &currentDecodingTest->timedTests[i];
-      snprintf(unityMessage, unityMessageLength, "%d %s", i, &timedTestTypeFriendlyName[i]);
-      UnityDefaultTestRun(currentDecodingTest->timedTests[i].run_test, &timedTestTypeFriendlyName[i], __LINE__);
+
+      //snprintf(unityMessage, unityMessageLength, "delta %u expected %u result %u", currentTimedTest->delta, currentTimedTest->expected, currentTimedTest->result);
+      //UnityMessage(unityMessage, __LINE__);
+
+      snprintf(unityMessage, unityMessageLength, "%d %s", i, timedTestTypeFriendlyName[i]);
+      UnityDefaultTestRun(run_test, unityMessage, __LINE__);
     }
 
     testDecoding_reset();
