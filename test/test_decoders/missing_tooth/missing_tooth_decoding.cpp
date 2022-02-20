0174f5f0da00e0097e4c405111851e5d61d39837
DeionSi <DeionSi@users.noreply.github.com>
2022-02-20 23:09:12 +0100

    Crank angle tests

--- test/test_decoders/missing_tooth/missing_tooth_decoding.cpp
+++ test/test_decoders/missing_tooth/missing_tooth_decoding.cpp
@@ -29,43 +29,77 @@ void test0_setup() {
   configPage10.TrigEdgeThrd = 0;
 }
 
-testParams test0_state0[] = { { testParams::SYNC, 0 }, { testParams::HALFSYNC, 0 } };
-testParams test0_state1[] = { { testParams::SYNC, 1 }, { testParams::HALFSYNC, 0 } };
-testParams test0_test0[] = { { testParams::REVCOUNT, 0 } };
+testParams test0_state0[] = {
+  { testParams::SYNC, 0 },
+  { testParams::HALFSYNC, 0 },
+  { testParams::SYNCLOSSCOUNT, 0 },
+};
+testParams test0_state1[] = {
+  { testParams::SYNC, 1 },
+  { testParams::HALFSYNC, 0 },
+  { testParams::SYNCLOSSCOUNT, 0 },
+};
+testParams test0_state2_tooth1[] = {
+  { testParams::SYNC, 1 },
+  { testParams::HALFSYNC, 0 },
+  { testParams::SYNCLOSSCOUNT, 0 },
+  { testParams::REVCOUNT, 1 },
+  { testParams::TOOTHANGLECORRECT, 1 }, // different
+  { testParams::TOOTHANGLE, 60 }, // different
+  { testParams::LASTTOOTHTIME, 0 }, // todo, calculate otf
+  { testParams::LASTTOOTHTIMEMINUSONE, 0 }, // todo, calculate otf
+  { testParams::RPM, 0 }, // todo, calculate otf
+  { testParams::STALLTIME, 0 }, // todo
+  { testParams::CRANKANGLE, 0 },
+};
+testParams test0_state2[] = {
+  { testParams::SYNC, 1 },
+  { testParams::HALFSYNC, 0 },
+  { testParams::SYNCLOSSCOUNT, 0 },
+  { testParams::REVCOUNT, 1 },
+  { testParams::TOOTHANGLECORRECT, 1 },
+  { testParams::TOOTHANGLE, 30 },
+  { testParams::LASTTOOTHTIME, 0 }, // todo, calculate otf
+  { testParams::LASTTOOTHTIMEMINUSONE, 0 }, // todo, calculate otf
+  { testParams::RPM, 0 }, // todo, calculate otf
+  { testParams::STALLTIME, 0 }, // todo
+  { testParams::CRANKANGLE, 0 },
+};
+testParams test0_crank[] = {
+  { testParams::CRANKANGLE, 0 }
+};
 
 //TODO: a stalling test to verify all parameters are returned
 //TODO: Cranking tests
 timedEvent test0_events[] {
-  { timedEvent::PRITRIG, 1000,     timedEventArrayTestEntry(test0_state0), 1000UL*0 }, // 1
-  { timedEvent::PRITRIG, 2000,     timedEventArrayTestEntry(test0_state0), 1000UL*30 }, // 2
-  { timedEvent::PRITRIG, 3000,     timedEventArrayTestEntry(test0_state0), 1000UL*60 }, // 3
-  { timedEvent::PRITRIG, 4000,     timedEventArrayTestEntry(test0_state0), 1000UL*90 }, // 4
-  { timedEvent::PRITRIG, 5000,     timedEventArrayTestEntry(test0_state0), 1000UL*120 }, // 5
-  { timedEvent::PRITRIG, 6000,     timedEventArrayTestEntry(test0_state0), 1000UL*150 }, // 6
-  { timedEvent::PRITRIG, 7000,     timedEventArrayTestEntry(test0_state0), 1000UL*180 }, // 7
-  { timedEvent::PRITRIG, 8000,     timedEventArrayTestEntry(test0_state0), 1000UL*210 }, // 8
-  { timedEvent::PRITRIG, 9000,     timedEventArrayTestEntry(test0_state0), 1000UL*240 }, // 9
-  { timedEvent::PRITRIG, 10000,    timedEventArrayTestEntry(test0_state0), 1000UL*270  }, // 10
-  { timedEvent::PRITRIG, 11000,    timedEventArrayTestEntry(test0_state0), 1000UL*300 }, // 11
-  { timedEvent::PRITRIG, 13000,    timedEventArrayTestEntry(test0_state0), 1000UL*0 }, // 1 (incl missing tooth 12)
-  { timedEvent::TEST,    13700,    timedEventArrayTestEntry(test0_test0),  1000UL*30 },
-  { timedEvent::PRITRIG, 14000,    timedEventArrayTestEntry(test0_state1), 1000UL*60 }, // 2
-  { timedEvent::PRITRIG, 15000,    timedEventArrayTestEntry(test0_state1), 1000UL*90 }, // 3
-  { timedEvent::PRITRIG, 16000,    timedEventArrayTestEntry(test0_state1), 1000UL*120 }, // 4
-/*  { .type = timedEvent::PRITRIG, .time = 17000,    .test = nullptr }, // 5
-  { .type = timedEvent::PRITRIG, .time = 18000,    .test = nullptr }, // 6
-  { .type = timedEvent::PRITRIG, .time = 19000,    .test = nullptr }, // 7
-  { .type = timedEvent::PRITRIG, .time = 20000,    .test = nullptr }, // 8
-  { .type = timedEvent::PRITRIG, .time = 21000,    .test = nullptr }, // 9
-  { .type = timedEvent::PRITRIG, .time = 22000,    .test = nullptr }, // 10
-  { .type = timedEvent::PRITRIG, .time = 23000,    .test = nullptr }, // 11
-  { .type = timedEvent::PRITRIG, .time = 25000,    .test = nullptr }, // 12+1 Missing tooth
-  { .type = timedEvent::TEST,           .time = 25500,    .test = new testParams ( testParams::CRANKANGLE, 15) },
-  { .type = timedEvent::PRITRIG, .time = 26000,    .test = nullptr }, // 2
-  { .type = timedEvent::TEST,           .time = 26500,    .test = new testParams ( testParams::CRANKANGLE, 45) },
-  { .type = timedEvent::PRITRIG, .time = 27000,    .test = nullptr }, // 3
-  { .type = timedEvent::PRITRIG, .time = 28000,    .test = nullptr }, // 4
-  { .type = timedEvent::PRITRIG, .time = 29000,    .test = nullptr }, // 5
+  { timedEvent::PRITRIG, 1000,     timedEventArrayTestEntry(test0_state0), 0 }, // 1
+  { timedEvent::PRITRIG, 2000,     timedEventArrayTestEntry(test0_state0), 30 }, // 2
+  { timedEvent::PRITRIG, 3000,     timedEventArrayTestEntry(test0_state0), 60 }, // 3
+  { timedEvent::PRITRIG, 4000,     timedEventArrayTestEntry(test0_state0), 90 }, // 4
+  { timedEvent::PRITRIG, 5000,     timedEventArrayTestEntry(test0_state0), 120 }, // 5
+  { timedEvent::PRITRIG, 6000,     timedEventArrayTestEntry(test0_state0), 150 }, // 6
+  { timedEvent::PRITRIG, 7000,     timedEventArrayTestEntry(test0_state0), 180 }, // 7
+  { timedEvent::PRITRIG, 8000,     timedEventArrayTestEntry(test0_state0), 210 }, // 8
+  { timedEvent::PRITRIG, 9000,     timedEventArrayTestEntry(test0_state0), 240 }, // 9
+  { timedEvent::PRITRIG, 10000,    timedEventArrayTestEntry(test0_state0), 270  }, // 10
+  { timedEvent::PRITRIG, 11000,    timedEventArrayTestEntry(test0_state0), 300 }, // 11
+  { timedEvent::PRITRIG, 13000,    timedEventArrayTestEntry(test0_state0), 0 }, // 1 (incl missing tooth 12)
+  { timedEvent::PRITRIG, 14000,    timedEventArrayTestEntry(test0_state1), 30 }, // 2
+  { timedEvent::PRITRIG, 15000,    timedEventArrayTestEntry(test0_state1), 60 }, // 3
+  { timedEvent::PRITRIG, 16000,    timedEventArrayTestEntry(test0_state1), 90  }, // 4
+  { timedEvent::PRITRIG, 17000,    timedEventArrayTestEntry(test0_state1), 120 }, // 
+  { timedEvent::PRITRIG, 18000,    timedEventArrayTestEntry(test0_state1), 150 }, // 6
+  { timedEvent::PRITRIG, 19000,    timedEventArrayTestEntry(test0_state1), 180 }, // 7
+  { timedEvent::PRITRIG, 20000,    timedEventArrayTestEntry(test0_state1), 210 }, // 8
+  { timedEvent::PRITRIG, 21000,    timedEventArrayTestEntry(test0_state1), 240 }, // 9
+  { timedEvent::PRITRIG, 22000,    timedEventArrayTestEntry(test0_state1), 270 }, // 10
+  { timedEvent::PRITRIG, 23000,    timedEventArrayTestEntry(test0_state1), 300 }, // 11
+  { timedEvent::PRITRIG, 25000,    timedEventArrayTestEntry(test0_state2_tooth1), 0 }, // 12+1 // TODO Missing tooth //this doesn't fail with 0 ????????
+  { timedEvent::TEST,    25500,    timedEventArrayTestEntry(test0_crank),  15 },
+  { timedEvent::PRITRIG, 26000,    timedEventArrayTestEntry(test0_state2), 30 }, // 2
+  { timedEvent::PRITRIG, 27000,    timedEventArrayTestEntry(test0_state2), 60 }, // 3
+  { timedEvent::PRITRIG, 28000,    timedEventArrayTestEntry(test0_state2), 90 }, // 4
+/*  { .type = timedEvent::PRITRIG, .time = 29000,    .test = nullptr }, // 5
   { .type = timedEvent::PRITRIG, .time = 30000,    .test = nullptr }, // 6
   { .type = timedEvent::PRITRIG, .time = 31000,    .test = nullptr }, // 7
   { .type = timedEvent::PRITRIG, .time = 32000,    .test = nullptr }, // 8
